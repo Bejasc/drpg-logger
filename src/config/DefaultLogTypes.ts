4c325177c5db5f6c11dd578aada41bfdf217978b
Bejasc <benjamin.schulz94@gmail.com>
2022-11-05 16:37:46 +1030

    re-add drpg-utils dependency

--- src/config/DefaultLogTypes.ts
+++ src/config/DefaultLogTypes.ts
@@ -1,17 +1,6 @@
 import { bgRed, cyan, gray, magenta, red, yellow } from "colorette";
+import { DrpgColors } from "drpg-utils";
 import { ILogType } from "../types/ILogType";
-
-export const DrpgColors = {
-	green: "#4CAF50",
-	white: "#EEEEEE",
-	yellow: "#FFEA00",
-	blue: "#64B5F6",
-	orange: "#FF6F00",
-	red: "#B71C1C",
-	black: "#263238",
-	pink: "#E040FB",
-	purple: "#6A1B9A",
-};
 
 export const LogLevel = {
 	Trace: {
