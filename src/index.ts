0449ec44c2b047b3e21d1f44395d85628472da93
Bejasc <benjamin.schulz94@gmail.com>
2022-11-05 16:03:06 +1030

    Removed reference to drpg-utils, added DrpgColors directly

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
-import { DrpgColors } from "drpg-utils";
-import { Logger } from "./Logger";
-
 export * from "./Logger";
 export * from "./config/DefaultLogTypes";
 export * from "./types/IEmbedOptions";
 export * from "./types/ILogType";
 export * from "./types/ILoggerOptions";
-
-Logger.debug("Hello World");
