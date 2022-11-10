93c951e5357267ba1e89c6568b65d86be12b6a25
Ben <benjamin.schulz94@gmail.com>
2022-11-10 21:10:12 +1030

    fix: add coloretteResolver methods to index

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from "./config/DefaultLogTypes";
 export * from "./types/IEmbedOptions";
 export * from "./types/ILogType";
 export * from "./types/ILoggerOptions";
+export * from "./functions/coloretteResolver";
